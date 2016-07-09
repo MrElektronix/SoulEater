0e5a9ac064073debbb45f2a910b5ddf298da3e3b
MrElektronix <milovanpelt@gmail.com>
2016-07-09 10:20:58 +0200

    SoulEater 9 Juli 10:20
    - added player object
    - draw player on canvas

--- Game.js
+++ Game.js
@@ -1,5 +1,37 @@
 window.addEventListener("load",function(){
+
   canvas = document.getElementById("canvas");
   ctx = canvas.getContext("2d");
 
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+  function resizeCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;;
+  }
+
+  window.addEventListener("resize", resizeCanvas, false);
+  resizeCanvas();
+
+  knight = new Player(300, 300);
+
 });
+
+var Player = function(x, y) {
+  this.x = x;
+  this.y = y;
+  this.width = 80;
+  this.height = 100;
+  //this.health = health;
+  //this.attacks = 10;
+  this.render = function() {
+    ctx.clearRect(this.x, this.y, this.width, this.height);
+    ctx.fillStyle = "red";
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+  this.update = function() {
+
+  }
+  setInterval(this.render(), 10);
+}

458730c124bcfdbeecf256820303246033348a11
MrElektronix <milovanpelt@gmail.com>
2016-07-24 19:40:13 +0200

    SoulEater 24 Juli 19:40
    - Added knight sprite

--- Game.js
+++ Game.js
@@ -3,12 +3,12 @@ window.addEventListener("load",function(){
   canvas = document.getElementById("canvas");
   ctx = canvas.getContext("2d");
 
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
+  canvas.width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  canvas.height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
 
   function resizeCanvas() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;;
+    canvas.width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    canvas.height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
   }
 
   window.addEventListener("resize", resizeCanvas);
@@ -26,17 +26,26 @@ window.addEventListener("load",function(){
 
 
   LeftKey = 37;
-  UpKey = 38;
+  //UpKey = 38;
   RightKey = 39;
-  DownKey = 40;
+  //DownKey = 40;
 
   A_LeftKey = 65;
-  S_DownKey = 83;
-  W_UpKey = 87;
+  //S_DownKey = 83;
+  //W_UpKey = 87;
   D_RightKey = 68;
 
-  var knight = new Component(300, 768, 80, 100, "red", 100, 10, 5, true);
-  var swine = new Component(1300, 768, 80, 100, "blue", 50, 20, 0, false);
+
+  var knight = new Component(300, canvas.height - 180, 80, 100, "red", 100, 10, 5, true, true);
+  var swine = new Component(1300, canvas.height - 180, 80, 100, "blue", 50, 20, 0, false, false);
+
+  knightSprite = new Image();
+  knightSprite.src = "knight_sprite.png";
+
+  knightSprite.addEventListener("load", function(){
+    setInterval(animate, 10);
+  });
+
 
   function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -46,12 +55,9 @@ window.addEventListener("load",function(){
     swine.update();
     swine.draw();
   }
-  setInterval(animate, 10);
 });
 
-
-
-function Component(x, y, width, height, color, health, attack, speed, useKeys) {
+function Component(x, y, width, height, color, health, attack, speed, useKeys, hasImage) {
   var self = this;
   this.x = x;
   this.y = y;
@@ -62,6 +68,7 @@ function Component(x, y, width, height, color, health, attack, speed, useKeys) {
   this.attack = attack;
   this.speed = speed;
   this.useKeys = useKeys;
+  this.hasImage = hasImage;
   this.update = function() {
     if (this.x < 0) {
       this.x = 0;
@@ -90,39 +97,49 @@ function Component(x, y, width, height, color, health, attack, speed, useKeys) {
 
   }
   this.draw = function() {
-    ctx.fillStyle = this.color;
-    ctx.fillRect(this.x, this.y, this.width, this.height);
-    this.healthBar(this.health);
+    if (this.hasImage) {
+      ctx.drawImage(knightSprite,this.x, this.y, this.width, this.height);
+      this.healthBar(this.health);
+    } else {
+      ctx.fillStyle = this.color;
+      ctx.fillRect(this.x, this.y, this.width, this.height);
+      this.healthBar(this.health);
+    }
   }
 
   this.healthBar = function(health) {
       ctx.beginPath();
+      ctx.font = "30px Arial";
       x = this.x;
       y = this.y - 60;
       health = this.health;
       width = this.health * 2;
       thickness = 15;
+      ctx.lineWidth = 3;
+      ctx.fillStyle = "black";
+      ctx.fillText(health,x, y-10);
+      ctx.strokeRect(x, y, width, thickness);
       ctx.rect(x, y, width, thickness)
       if(health > 43){
           ctx.fillStyle = "green"
-      }else if(health > 27){
+      } else if(health > 27){
         ctx.fillStyle = "orange";
-      }else{
-        ctx.fillStyle="red";
+      } else{
+        ctx.fillStyle = "red";
       }
       ctx.closePath();
       ctx.fill();
   }
 
-    window.onclick = function(e){
-      if (e.button == 0) {
-        self.health -= 10;
-        if (self.health <= 0) {
-          self.health = 0;
-          setTimeout(function(){
-            alert("Enemy Dead");
-          }, 60)
-        }
+  window.onclick = function(e){
+    if (e.button == 0) {
+      self.health -= 10;
+      if (self.health <= 0) {
+        self.health = 0;
+        setTimeout(function(){
+          alert("Enemy Dead");
+        }, 60)
       }
     }
+  }
 }

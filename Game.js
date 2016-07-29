22f1ff2b26699a2838812352a090759e204df276
MrElektronix <milovanpelt@gmail.com>
2016-07-29 15:25:21 +0200

    SoulEater 29 Juli 15:25
    - Rewritten the code

--- Game.js
+++ Game.js
@@ -1,74 +1,79 @@
-window.addEventListener("load",function(){
 
-  canvas = document.getElementById("canvas");
-  ctx = canvas.getContext("2d");
-
-  canvas.width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-  canvas.height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
-
-  function resizeCanvas() {
-    canvas.width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-    canvas.height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
-  }
-
-  window.addEventListener("resize", resizeCanvas);
-  resizeCanvas();
-
-  key = [];
-
-  document.addEventListener("keydown", function(e){
-    key[e.keyCode] = true;
-  });
-
-  document.addEventListener("keyup", function(e){
-    delete key[e.keyCode];
-  });
-
-
-  LeftKey = 37;
-  //UpKey = 38;
-  RightKey = 39;
-  //DownKey = 40;
-
-  A_LeftKey = 65;
-  //S_DownKey = 83;
-  //W_UpKey = 87;
-  D_RightKey = 68;
-
-
-  var knight = new Component(300, canvas.height - 180, 80, 100, "red", 100, 10, 5, true, true);
-  var swine = new Component(1300, canvas.height - 180, 80, 100, "blue", 50, 20, 0, false, false);
-
-  knightSprite = new Image();
-  knightSprite.src = "knight_sprite.png";
-
-  knightSprite.addEventListener("load", function(){
-    setInterval(animate, 10);
-  });
-
-
-  function animate() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    knight.update()
-    knight.draw();
-
-    swine.update();
-    swine.draw();
-  }
+window.addEventListener("load", function(){
+  StartGame();
 });
 
-function Component(x, y, width, height, color, health, attack, speed, useKeys, hasImage) {
-  var self = this;
-  this.x = x;
-  this.y = y;
+var Knight, Swine;
+
+function StartGame() {
+  Game.start();
+  Knight = new Component(50, (Game.canvas.height - 200), 100, 100, "black", 5, 100, 5,"Player");
+  Swine = new Component((Game.canvas.width - 400), (Game.canvas.height - 200), 100, 100, "green", 5, 50, 15,"Enemy");
+}
+
+var Game = {
+  canvas: document.createElement("canvas"),
+  start: function() {
+    this.canvas.width = 1200;
+    this.canvas.height = 800;
+    this.ctx = this.canvas.getContext("2d");
+    document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    this.interval = setInterval(UpdateGame, 10);
+    key = [];
+    document.addEventListener("keydown", function(e){
+      key[e.keyCode] = true;
+    });
+
+    document.addEventListener("keyup", function(e){
+      delete key[e.keyCode];
+    });
+
+    LeftKey = 37;
+    //UpKey = 38;
+    RightKey = 39;
+    //DownKey = 40;
+
+    A_LeftKey = 65;
+    //S_DownKey = 83;
+    //W_UpKey = 87;
+    D_RightKey = 68;
+  },
+  clear: function() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  }
+}
+
+function Component(xpos, ypos, width, height, color, speed, health, attack, type) {
+  this.x = xpos;
+  this.y = ypos;
   this.width = width;
   this.height = height;
   this.color = color;
+  this.speed = speed;
   this.health = health;
   this.attack = attack;
-  this.speed = speed;
-  this.useKeys = useKeys;
-  this.hasImage = hasImage;
+  this.Type = type;
+  this.draw = function() {
+    ctx = Game.ctx;
+    if (this.Type == "Player") {
+      ctx.fillStyle = color;
+      ctx.fillRect(this.x, this.y, this.width, this.height);
+      this.healthBar(this.health);
+      ctx.beginPath();
+      ctx.fillStyle = "white";
+      ctx.arc(this.x + 20, this.y + 20, 7, 0, 2 * Math.PI);
+      ctx.arc(this.x + 60, this.y + 20, 7, 0, 2 * Math.PI);
+      ctx.fill();
+      ctx.beginPath();
+      ctx.arc(this.x + 40, this.y + 45, 25, 0, 1 * Math.PI);
+      ctx.fill();
+    }
+    else if (this.Type == "Enemy") {
+      ctx.fillStyle = color;
+      ctx.fillRect(this.x, this.y, this.width, this.height);
+      this.healthBar(this.health);
+    }
+  }
   this.update = function() {
     if (this.x < 0) {
       this.x = 0;
@@ -76,34 +81,11 @@ function Component(x, y, width, height, color, health, attack, speed, useKeys, h
     if (this.y < 0) {
       this.y = 0;
     }
-    if (this.x > (canvas.width - this.width)) {
-      this.x = canvas.width - this.width;
+    if (this.x > (Game.canvas.width - this.width)) {
+      this.x = Game.canvas.width - this.width;
     }
-    if (this.y > (canvas.height - this.height)) {
-      this.y = canvas.height - this.height;
-    }
-
-    this.Keys = function() {
-      if (this.useKeys == true) {
-        if (key[A_LeftKey] || key[LeftKey]) {
-          this.x -= this.speed;
-        }
-        if (key[D_RightKey] || key[RightKey]) {
-          this.x += this.speed;
-        }
-      }
-    }
-    this.Keys();
-
-  }
-  this.draw = function() {
-    if (this.hasImage) {
-      ctx.drawImage(knightSprite,this.x, this.y, this.width, this.height);
-      this.healthBar(this.health);
-    } else {
-      ctx.fillStyle = this.color;
-      ctx.fillRect(this.x, this.y, this.width, this.height);
-      this.healthBar(this.health);
+    if (this.y > (Game.canvas.height - this.height)) {
+      this.y = Game.canvas.height - this.height;
     }
   }
 
@@ -130,16 +112,51 @@ function Component(x, y, width, height, color, health, attack, speed, useKeys, h
       ctx.closePath();
       ctx.fill();
   }
+}
 
-  window.onclick = function(e){
-    if (e.button == 0) {
-      self.health -= 10;
-      if (self.health <= 0) {
-        self.health = 0;
-        setTimeout(function(){
-          alert("Enemy Dead");
-        }, 60)
-      }
-    }
+window.onclick = function(target){
+  target = Swine;
+  console.log("hi");
+  target.health -= 10;
+  console.log(target.health);
+  if (target.health <= 0) {
+    target.health = 0;
+    setTimeout(function(){
+    alert("Enemy Dead");
+    }, 60)
   }
 }
+
+
+Component.prototype.keys = function() {
+    if (key[A_LeftKey] || key[LeftKey]) {
+      this.x -= this.speed;
+    }
+    if (key[D_RightKey] || key[RightKey]) {
+      this.x += this.speed;
+    }
+};
+
+function UpdateGame() {
+  Game.clear();
+  Knight.keys();
+  Knight.update();
+  Swine.update();
+
+  Knight.draw();
+  Swine.draw();
+}
+
+
+/*
+  canvas.width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  canvas.height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+
+  function resizeCanvas() {
+    canvas.width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    canvas.height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+  }
+
+  window.addEventListener("resize", resizeCanvas);
+  resizeCanvas();
+*/
